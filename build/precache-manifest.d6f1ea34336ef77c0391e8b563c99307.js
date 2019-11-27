168545d9ab9914feb1c3f85093bd9134779c546b
Vitaliy Vaskiv <iamvaskiv@gmail.com>
2019-11-27 22:24:19 +0200

    build

---
+++ build/precache-manifest.d6f1ea34336ef77c0391e8b563c99307.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62cad4f6182b6d34f2e7058fc9fcee5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11d60e8d930df7809ede",
+    "url": "/static/css/main.dd088e14.chunk.css"
+  },
+  {
+    "revision": "c5344337ead6a8da259c",
+    "url": "/static/js/2.c51b25bf.chunk.js"
+  },
+  {
+    "revision": "11d60e8d930df7809ede",
+    "url": "/static/js/main.0489e882.chunk.js"
+  },
+  {
+    "revision": "1eff351edcce9dbcd0b6",
+    "url": "/static/js/runtime-main.ff5fa7e4.js"
+  }
+]);
